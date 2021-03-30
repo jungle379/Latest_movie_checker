c92481b5c5cfa88a5d3e78d8e5367553fd42720b
jungle379 <jungle379@gmail.com>
2021-03-30 23:24:59 +0900

    ✨ CSS修正1

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,42 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="toppage">
       <Head>
         <title>Latest_Movie_Checker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="h-screen bg-green-100">
-        <h1 className="container mx-auto text-6xl font-thin text-red-400 underline mb-60">
+        <h1 className="text-center container mx-auto text-6xl font-thin text-red-400 underline mb-40">
         <a href="">Latest Movie Checker</a>
         </h1>
 
-        <div className="flex">
+        <div className="flex h-20">
           <Link href="/newest/latest">
-            <h2 className="bg-center text-2xl text-blue-300 justify-between mr-10">最新の映画</h2>
+            <h2 className="bg-center text-2xl text-blue-300 justify-between ml-20 mr-10">最新の映画</h2>
           </Link>
           <Link href="/newest/nextweek">
-            <h3 className="bg-center text-2xl text-blue-300 justify-between mr-10">来週公開予定の映画</h3>
+            <h3 className="bg-center text-2xl text-blue-300 justify-between ml-20 mr-10">来週公開予定の映画</h3>
           </Link>
         </div>
       </main>
 
-      <footer className="bg-bottom text-0.5xl flex h-300">
+      <footer className="bg-bottom text-0.5xl h-300">
+        <div className="flex">
         <Link href="">
-          <h4 className="justify-between pl-20">初めての方へ</h4>
+          <h4 className="flex-auto pl-20 ml-20 w-10">初めての方へ</h4>
         </Link>
         <Link href="">
-          <h5 className="justify-between pl-40">作成者について</h5>
+          <h5 className="flex-auto pl-20 ml-20 w-10">作成者について</h5>
         </Link>
-        <Link href="">
-          <h6 className="justify-between pl-40">・・・</h6>
-        </Link>
-      </footer>             
+        </div>
       <Link href="">
-           To_movie_fun.inc
+           <div className="ml-80 pl-80">To_movie_fun.inc</div>
           </Link>
+      </footer>
     </div>
   )
 }
---
+++ pages/lib/posts.js
@@ -0,0 +1,83 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import remark from 'remark'
+import html from 'remark-html'
+
+const postsDirectory = path.join(process.cwd(), 'posts')
+
+export function getSortedPostsData() {
+  // /posts　配下のファイル名を取得する
+  const fileNames = fs.readdirSync(postsDirectory)
+  const allPostsData = fileNames.map(fileName => {
+    // id を取得するためにファイル名から ".md" を削除する
+    const id = fileName.replace(/\.md$/, '')
+
+    // マークダウンファイルを文字列として読み取る
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    // 投稿のメタデータ部分を解析するために gray-matter を使う
+    const matterResult = matter(fileContents)
+
+    // データを id と合わせる
+    return {
+      id,
+      ...matterResult.data
+    }
+  })
+  // 投稿を日付でソートする
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1
+    } else {
+      return -1
+    }
+  })
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory)
+
+  // 以下のような配列を返します:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  })
+}
+
+export async function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  // 投稿のメタデータ部分を解析するために gray-matter を使う
+  const matterResult = matter(fileContents)
+
+  // マークダウンを HTML 文字列に変換するために remark を使う
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
+  // データを id および contentHtml と組み合わせる
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data
+  }
+}
