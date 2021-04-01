dadea9660b3e9b402f0a6ff8ae0cd8ee20364058
jungle379 <jungle379@gmail.com>
2021-04-01 22:47:06 +0900

    🎉 add_md

--- pages/newest/latest.js
+++ pages/newest/latest.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { getPostData } from '../../lib/posts'
 
 export default function FirstPost() {
   return (
@@ -7,6 +8,25 @@ export default function FirstPost() {
     <Head>
       <title>最新情報</title>
     </Head>
+    <article>
+      <div dangerouslySetInnerHTML={{ __html: getPostData.contentHtml }} />
+    </article>
     </>
   )
+}
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
