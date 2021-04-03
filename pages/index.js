ffdf67352245336fc90a1d65398f3a079bb58f05
jungle379 <jungle379@gmail.com>
2021-04-03 23:32:07 +0900

    ✅ 1週分の総括

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className="h-screen bg-green-100">
         <h1 className="container ml-10 text-6xl font-thin text-red-400 underline mb-40">
-        <a href="">Latest Movie Checker</a>
+        <Link href="">Latest Movie Checker</Link>
         </h1>
 
         <div className="flex h-10">
@@ -20,6 +20,9 @@ export default function Home() {
           </Link>
           <Link href="/newest/nextweek">
             <h3 className="bg-center text-2xl text-blue-300 justify-between ml-20 mr-10">来週公開予定の映画</h3>
+          </Link>
+          <Link href="/newest/nextweek">
+            <h3 className="bg-center text-2xl text-blue-300 justify-between ml-20 mr-10">今月公開予定の映画</h3>
           </Link>
         </div>
       </main>
--- posts/first.md
+++ posts/first.md
@@ -1,1 +1,1 @@
-test
+Title:test
