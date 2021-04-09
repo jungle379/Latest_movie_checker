f858c5ea25b46bb90d66ae32dd15ab97693cbe02
jungle379 <jungle379@gmail.com>
2021-04-09 22:13:52 +0900

    id修正

--- pages/newest/latest.js
+++ pages/newest/latest.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { Footer } from '../../components/footer'
 import { Header } from '../../components/header'
+import { getSortedPostsData } from '../lib/posts'
 
-
-export default function Post({ allPostsData }) {
+export default function Home({ allPostsData }) {
   return (
     <>
       <Head>
@@ -13,28 +13,25 @@ export default function Post({ allPostsData }) {
       <main className="h-screen bg-green-100">
       <Header></Header>
       <h1 className="text-5xl pb-5 text-red-700 ml-20">今週公開の映画一覧</h1>
-      <Link href={'/posts/${id}'}>
-        <a className="">title</a>
-      </Link>
+      {allPostsData.map(({id, title}) => {
+        return (
+          <li className="" key={id}>
+          <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            </li>
+        )
+      })}
       </main>
       <Footer></Footer>
       </>
   )
 }
-
-// export async function getStaticPaths() {
-//   const paths = getAllPostIds()
-//   return {
-//     paths,
-//     fallback: false
-//   }
-// }
-
-// export async function getStaticProps({ params }) {
-//   const allPostsData = getPostData(params.id)
-//   return {
-//     props: {
-//       postData
-//     }
-//   }
-// }
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
--- posts/first.md
+++ posts/first.md
@@ -1,5 +1,5 @@
 ---
-title: 'Two Forms of Pre-rendering'
+title: 'First'
 date: '2020-01-01'
 ---
 
