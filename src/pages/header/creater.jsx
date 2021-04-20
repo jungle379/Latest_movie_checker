717d04b9408efc7ea83667d530099415198e815e
jungle379 <jungle379@gmail.com>
2021-04-20 12:35:48 +0900

    ⚡️ ルート修正

--- src/pages/header/creater.jsx
+++ src/pages/header/creater.jsx
@@ -13,13 +13,15 @@ export default function Creater() {
     </Head>
     <main className="h-screen bg-green-100">
       <Header></Header>
-    <div className="text-3xl ml-20 text-red-500">作成者について</div>
-    <h1 className="text-2xl mt-10 ml-40">森野　弘規</h1>
-    <div className="text-2xl text-blue-500 ml-40 mt-20">
+    <div className="text-3xl ml-20 text-red-500">作成者について
+    <h1 className="text-2xl mt-10 ml-20 text-black">森野　弘規</h1>
+    <h2 className="text-2xl ml-20 mt-20 text-black">・SNSリンク</h2>
+    </div>
+    <div className="text-2xl text-blue-500 ml-40 mt-10">
     <FontAwesomeIcon icon={faTwitterSquare} />
     <Link href="https://twitter.com/hirokimorino">twitter</Link>
     </div>
-    <div className="text-2xl text-blue-500 ml-40 mt-20">
+    <div className="text-2xl text-blue-700 ml-40 mt-20">
     <FontAwesomeIcon icon={faFacebookSquare} />
     <Link href="https://www.facebook.com/hiroki.morino.9/">facebook</Link>
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,7 +24,7 @@ export default function Home() {
           </Link>
         </div>
           <Link href="/near/map">
-            <h4 className="text-red-500 underline text-2xl ml-20 mt-20">現在地から近くの映画館</h4>
+            <h4 className="text-red-500 underline text-2xl ml-20 mt-20">現在地から近くの映画館を探す</h4>
           </Link>
         <h5 className="text-0.5xl text-yellow-800 mt-10 ml-20">uploaded in 2021/04/15</h5>
       </main>
