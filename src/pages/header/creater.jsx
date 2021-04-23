ddb0c3e5387205d9956518048c350543f039fbd1
jungle379 <jungle379@gmail.com>
2021-04-23 12:45:43 +0900

    🐛 🔥 紹介ページ追記

--- src/pages/header/creater.jsx
+++ src/pages/header/creater.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { Footer } from '../../components/footer'
 import { Header } from '../../components/header'
+import { Introduce } from '../../components/introduce'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
@@ -12,8 +13,10 @@ export default function Creater() {
     <main className="h-screen bg-green-100">
       <Header></Header>
     <div className="text-3xl ml-20 text-red-500">作成者について
-    <h1 className="text-2xl mt-10 ml-20 text-black">森野　弘規</h1>
-    <h2 className="text-2xl ml-20 mt-20 text-black">・SNSリンク</h2>
+    <h1 className="text-4xl mt-10 ml-10 text-black">森野　弘規</h1>
+    <Introduce></Introduce>
+    </div>
+    <div className="text-3xl ml-20 mt-10 text-red-500">SNSリンク
     </div>
     <div className="text-2xl text-blue-500 ml-40 mt-10 flex">
     <FontAwesomeIcon className="h-10 mr-5" icon={faTwitterSquare} />
