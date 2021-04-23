59e704de01430352ba71c1be0b2678b8da393642
jungle379 <jungle379@gmail.com>
2021-04-23 12:21:34 +0900

    ✨ mapのベース作成

--- src/pages/near/map.jsx
+++ src/pages/near/map.jsx
@@ -1,19 +1,25 @@
-import Head from 'next/head'
-import Link from 'next/link'
 import { Footer } from '../../components/footer'
 import { Header } from '../../components/header'
 
 export default function FirstPost() {
   return (
     <>
-    <Head>
-      <title>映画館検索</title>
-    </Head>
+    <title>映画館検索</title>
     <main className="h-screen bg-green-100">
     <Header></Header>
     <h1 className="text-5xl pb-5 text-red-700 ml-20">近場の映画館検索</h1>
+    <div className="" id="map">
+
+    </div>
     </main>
     <Footer></Footer>
     </>
   )
 }
+
+// const map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'),{
+
+//   });
+// }
