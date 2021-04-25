46c00e228202099f499f2f18f49289e60a838720
jungle379 <jungle379@gmail.com>
2021-04-25 21:54:11 +0900

    🐛

--- src/pages/near/map.jsx
+++ src/pages/near/map.jsx
@@ -7,9 +7,9 @@ export default function FirstPost() {
     <title>映画館検索</title>
     <main className="h-screen bg-green-100">
     <Header></Header>
-    <h1 className="text-5xl pb-5 text-red-700 ml-20">近場の映画館検索</h1>
-    <div className="" id="map">
-
+    <div className="text-5xl pb-5 text-red-700 ml-20">近場の映画館検索</div>
+    <div className="ml-40" id="map">
+      地図API未取得で、未実装となります。
     </div>
     </main>
     <Footer></Footer>
