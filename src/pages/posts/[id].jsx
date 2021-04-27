0806eb723afd3d0855def37fe84a7f67e38a0734
jungle379 <jungle379@gmail.com>
2021-04-27 12:32:59 +0900

    🐛 jsxに変更

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,21 +1,18 @@
 import { getAllPostIds, getPostData } from '../../../lib/posts'
-import Head from 'next/head'
 import Date from '../../components/date'
 
 export default function Post({ postData }) {
   return (
     <>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article className="max-w-screen"> 
+      <title>{postData.title}</title>
+      <article className="max-w-screen">
         <h1 className="text-4xl bg-center">{postData.title}</h1>
         <div className="text-2xl text-red-800">
-        <Date dateString={postData.date} />
+          <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
-      </>
+    </>
   )
 }
 
