ad500b2d73ada92b15f492451636dd062a927492
jungle379 <jungle379@gmail.com>
2021-04-27 17:28:33 +0900

    ✨ pager追加

---
+++ src/pages/archive/[page].jsx
@@ -0,0 +1,70 @@
+import fs from "fs"
+import Link from "next/link"
+import Layout from "../../components/layout"
+import Pager from "../../components/pager"
+import { getSortedPostsData, listContentFiles } from "../../lib/posts"
+
+const COUNT_PER_PAGE = 10
+
+export default function Archive(props) {
+  const { posts, page, total, perPage } = props
+  return (
+    <Layout title="">
+      <h1>過去投稿一覧</h1>
+      {posts.map((post) => <div
+        key={post.id}
+        className="post-teaser"
+      >
+        <h2><Link href="/posts/[id]" as={`/posts/${post.id}`}><a>{post.title}</a></Link></h2>
+        <div><span>{post.published}</span></div>
+      </div>)}
+      <Pager
+        page={page} total={total} perPage={perPage}
+        href="/archive/[page]"
+        asCallback={(page) => `/archive/${page}`}
+      />
+      <style jsx>{`
+        .post-teaser {
+          margin-bottom: 2em;
+        }
+        .post-teaser h2 a {
+          text-decoration: none;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+/**
+ * ページコンポーネントで使用する値を用意する
+ */
+export async function getStaticProps({ params }) {
+  const page = parseInt(params.page, 10)
+  const end = COUNT_PER_PAGE * page
+  const start = end - COUNT_PER_PAGE
+  const posts = getSortedPostsData
+  return {
+    props: {
+      posts: posts(start, end),
+      page,
+      total: posts.length,
+      perPage: COUNT_PER_PAGE,
+    }
+  }
+}
+/**
+ * 有効な URL パラメータを全件返す
+ */
+export async function getStaticPaths() {
+  const posts = await listContentFiles({ fs })
+  const pages = range(Math.ceil(posts.length / COUNT_PER_PAGE))
+  const paths = pages.map((page) => ({
+    params: { page: `${page}` }
+  }))
+  return { paths: paths, fallback: false }
+}
+/**
+ * ユーティリティ: 1 から指定された整数までを格納した Array を返す
+ */
+function range(stop) {
+  return Array.from({ length: stop }, (_, i) => i + 1)
+}
