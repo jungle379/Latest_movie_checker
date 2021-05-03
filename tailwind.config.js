91649a4e8e6b4a6ffd7f5d3a67ee921f8151f7b5
jungle379 <jungle379@gmail.com>
2021-05-03 13:52:16 +0900

    CSS修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   //mode: "jit",
-  purge: ["./src/**/*.{ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx}"],
   darkMode: false, // 'media' or 'class'
   theme: { extend: {} },
   variants: { extend: {} },
