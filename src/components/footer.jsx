6fd2189cfad554e8c569d8203bc7824bfaf2d3e6
jungle379 <jungle379@gmail.com>
2021-05-07 12:48:51 +0900

    🔥 syuusei2

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div className="h-46 pt-10 text-0.5xl sm:bg-green-100">
       <Link href="">
-        <div className="m-3 sm:mt-40 leading-10 text-center">To_movie_fun.inc</div>
+        <div className="m-3 mt-40 leading-10 text-center">To_movie_fun.inc</div>
       </Link>
     </div>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <link rel="icon" href="/favicon.ico" />
       <main className="h-screen sm:bg-green-100">
         <Header></Header>
-        <div className="mt-36 flex h-16 bg-gray-300">
+        <div className="mt-36 flex h-16 sm:bg-gray-300">
           <Link href="/newest/latest">
             <h1 className="bg-center sm:text-2xl pt-5 hover:text-red-500 justify-between ml-20 mr-10">今週公開の映画</h1>
           </Link>
@@ -23,7 +23,9 @@ export default function Home() {
             <h3 className="bg-center sm:text-2xl pt-5 hover:text-red-500 justify-between ml-20 mr-10">現在地から近くの映画館を探す</h3>
           </Link>
         </div>
-        <h4 className="text-0.5xl text-yellow-800 sm:mt-40 ml-20">updated in 2021/04/15</h4>
+        <div>
+          <h4 className="text-0.5xl text-yellow-800 float-right mt-40 sm:mt-0.5 mr-20">updated in 2021/04/15</h4>
+        </div>
         <Footer></Footer>
       </main>
     </>
