b1fa60ce8baa184b401783d62600adab3d8de673
jungle379 <jungle379@gmail.com>
2021-05-11 12:28:13 +0900

    syuusei

--- src/pages/newest/latest.jsx
+++ src/pages/newest/latest.jsx
@@ -9,7 +9,7 @@ export default function Latest() {
       <main className="h-screen sm:bg-green-100">
         <Header></Header>
         <div>
-          <h1 className="sm:text-5xl pb-5 text-red-700 ml-10 sm:ml-20">今週公開の映画一覧</h1>
+          <h1 className="sm:text-5xl pb-5 text-red-700 mt-20 ml-10 sm:ml-20">今週公開の映画一覧</h1>
           <h2 className="sm:text-2xl mt-20 ml-10 sm:ml-40 text-indigo-500">
             <Link href="https://www.conan-movie.jp">・名探偵コナン(公開中)</Link></h2>
           <h3 className="sm:text-2xl mt-20 ml-10 sm:ml-40 text-indigo-500">
