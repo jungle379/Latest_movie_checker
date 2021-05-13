77f5251b8a4dfa58dfef3a5f3aaac34eda2e5e06
jungle379 <jungle379@gmail.com>
2021-05-13 12:24:04 +0900

    🔥 syuusei

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,21 +21,19 @@ export default function Home() {
             <h3 className="bg-center bg-gray-200 sm:text-2xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400">現在地から近くの映画館を探す</h3>
           </Link>
         </div>
-        <div>
-          <h4 className="text-0.5xl text-yellow-800 float-right mt-40 sm:mt-0.5 mr-20 border-red-600 ">updated in 2021/04/15</h4>
-        </div>
-        <div className="flex justify-around mt-20 h-60 border-dashed border-8 border-red-400 mr-40 ml-40">
+        <div className="flex justify-around mt-20 h-80 border-dashed border-8 border-red-400 mr-40 ml-40">
           <div className="text-2xl mt-10">最新情報
-          <h5 className="mt-10 text-xl">・4月の情報を更新しました。</h5>
+          <h4 className="mt-10 text-xl">
+              <Link href="/">・4月の情報を更新しました。</Link>
+            </h4>
           </div>
-          {/* <Link href="/">・4月の情報を更新しました。</Link> */}
           <div className="text-2xl mt-10">メンテ情報
-          <h6 className="text-xl mt-10">・特にありません</h6>
+          <h5 className="text-xl mt-10">・特にありません</h5>
           </div>
         </div>
-        {/* <div>
-          <img className="" src="/images/640.jpg" />
-        </div> */}
+        <div>
+          <h6 className="text-0.5xl text-yellow-800 float-right mt-40 sm:mt-0.5 mr-40 border-red-600 ">updated in 2021/04/15</h6>
+        </div>
         <Footer></Footer>
       </main>
     </>
