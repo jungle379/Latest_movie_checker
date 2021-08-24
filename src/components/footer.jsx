9008e022c9fc933d22cb456eaafdfff04eb7cd81
jungle379 <jungle379@gmail.com>
2021-08-24 23:22:03 +0900

    🔥

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div className="h-20 pt-10 text-0.5xl sm:bg-green-100">
       <Link href="/">
-        <div className="m-3 mt-10 leading-10 text-center sm:bg-green-100 bg-green-300">To_movie_fun.inc</div>
+        <div className="mt-10 leading-10 text-center sm:bg-green-100 bg-green-300">To_movie_fun.inc</div>
       </Link>
     </div>
   );
