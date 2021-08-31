57cbe908fbcf75d08fdc77ab8379bd0f6ab5bb3f
jungle379 <jungle379@gmail.com>
2021-08-31 22:23:05 +0900

    🔥 ボタンの修正

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,13 +7,13 @@ export function Main() {
       <Header />
       <div className="mt-20 flex h-20 justify-around">
         <Link href="/newest/latest">
-          <h1 className="bg-center bg-gray-200 sm:text-2xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400">今週公開の映画</h1>
+          <h1 className="bg-center w-1/3 text-center bg-gray-200 sm:text-4xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400">今週公開の映画</h1>
         </Link>
         <Link href="/archive/[page]" as="/archive/1">
-          <h2 className="bg-center bg-gray-200 sm:text-2xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400">今月公開予定の映画</h2>
+          <h2 className="bg-center w-1/3 text-center bg-gray-200 sm:text-4xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400">今月公開予定の映画</h2>
         </Link>
         <Link href="/near/map">
-          <h3 className="bg-center bg-gray-200 sm:text-2xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400">現在地から近くの映画館を探す</h3>
+          <h3 className="bg-center w-1/3 text-center bg-gray-200 sm:text-4xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400">近くの映画館を探す</h3>
         </Link>
       </div>
       <div className="flex justify-around mt-20 h-80 sm:border-dashed sm:border-8 sm:border-red-400 mr-40 ml-40">
