ec85a249d3ae16001a64b3a9f8844aae3137d273
jungle379 <jungle379@gmail.com>
2021-09-03 22:28:12 +0900

    ⚡️

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -6,27 +6,39 @@ import { Header } from "../components/header";
 const ITEMS = [
   {
     href: "/",
-    title: "・アクション映画"
+    title: "・アクション"
   }, {
     href: "/",
     title: "・ファンタジー"
   }, {
     href: "/",
     title: "・アニメ"
+  }, {
+    href: "/",
+    title: "・ラブコメ"
+  }, {
+    href: "/",
+    title: "・ドラマ"
+  }, {
+    href: "/",
+    title: "・ホラー"
+  }, {
+    href: "/",
+    title: "・サスペンス"
   }
 ];
 
 const Category = () => {
   return <>
     <Head>
-      <title>ジャンル別作品</title>
+      <title>ジャンル別オススメ作品</title>
     </Head>
     <div className="h-screen sm:bg-green-100 bg-green-300">
       <Header />
       <div>
         {ITEMS.map(item => {
           return (
-            <div className="text-2xl text-blue-500 pt-10 pl-10">
+            <div className="text-1xl text-blue-500 pt-10 pl-10 hover:text-red-600">
               <Link href={item.href}>
                 <a>{item.title}</a>
               </Link></div>
