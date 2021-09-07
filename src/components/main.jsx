11cdfdbdb018033fdd98de77a3f418f448b61dfa
jungle379 <jungle379@gmail.com>
2021-09-07 22:13:36 +0900

    🔥

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -22,17 +22,17 @@ export function Main() {
       <div className="sm:flex justify-around mt-20 h-80 sm:border-dashed sm:border-8 sm:border-red-400 mr-40 ml-40">
         <div>
           <h1 className="sm:text-4xl mt-10 ml-10">最新情報</h1>
-          <h2 className="mt-10 ml-14 sm:text-2xl w-100 text-blue-500 underline">
+          <h2 className="mt-10 sm:ml-14 sm:text-2xl sm:w-100 text-blue-500 underline">
             <Link href="/newest/news1">・8月の情報を更新しました。</Link>
           </h2>
-          <h3 className="mt-10 ml-14 sm:text-2xl w-100 text-blue-500 underline">
+          <h3 className="mt-10 sm:ml-14 sm:text-2xl sm:w-100 text-blue-500 underline">
             <Link href="/newest/co-news">・その他の最新情報</Link>
           </h3>
         </div>
-        <div className="sm:text-2xl w-60 ml-10 mt-10 sm:w-80 text-blue-500 underline">
+        <div className="sm:text-2xl w-60 sm:ml-10 mt-10 sm:w-80 text-blue-500 underline">
           <Link href="/maintainance">・メンテナンス情報</Link>
         </div>
-        <div className="sm:text-2xl w-60 ml-10 mt-10 sm:w-80 text-blue-500 underline">
+        <div className="sm:text-2xl w-60 sm:ml-10 mt-10 sm:w-80 text-blue-500 underline">
           <Link href="/news">・ニュース一覧</Link>
         </div>
       </div>
