789966cc9a8d3669f5f35269986febbb52c306ec
jungle379 <jungle379@gmail.com>
2021-09-09 05:26:26 +0900

    ⚡️

--- src/components/introduce.jsx
+++ src/components/introduce.jsx
@@ -1,9 +1,12 @@
 export function Introduce() {
   return (
     <div className="pt-10 sm:bg-green-100 bg-green-300">
-      <div className="m-3 ml-20 text-black text-2xl">経歴
+      <div className="m-3 ml-20 text-black sm:text-2xl">経歴
         <h1 className="text-base">2020/3よりプログラミング学習を開始し、日夜モダンな言語を中心に勉強中。</h1>
         <h2 className="text-base">使用可能言語：HTML、CSS、JS、RubyonRails、Next.js（当サイトもNext.jsを使用して作成しています。）</h2>
+      </div>
+      <div className="m-3 mt-10 ml-20 text-black sm:text-2xl">資格
+        <h1 className="text-base">・ITパスポート(現在、基本情報技術者、JavaSilverの勉強中)</h1>
       </div>
     </div>
   );
--- src/pages/header/creater.jsx
+++ src/pages/header/creater.jsx
@@ -13,20 +13,20 @@ export default function Creater() {
       <Head>
         <title>作成者情報</title>
       </Head>
-      <main className="h-screen sm:bg-green-100 bg-green-300">
+      <main className="h-auto sm:bg-green-100 bg-green-300">
         <Header />
-        <div className="text-3xl ml-20 mt-20 text-red-500">作成者について
-          <h1 className="text-2xl mt-10 ml-10 text-black">森野　弘規</h1>
+        <div className="sm:text-4xl ml-20 mt-20 text-red-500">作成者について
+          <h1 className="sm:text-4xl mt-10 ml-10 text-black">森野　弘規</h1>
           <Introduce />
         </div>
-        <div className="text-3xl ml-20 mt-10 text-red-500">SNS
+        <div className="sm:text-4xl ml-20 mt-10 text-red-500">SNS
         </div>
         <div className="text-2xl text-blue-500 ml-40 mt-10 flex">
           <FontAwesomeIcon className="h-10 mr-5" icon={faTwitterSquare} />
           <FontAwesomeIcon className="h-10 mr-5" icon={faChevronRight} />
           <Link href="https://twitter.com/hirokimorino">twitter</Link>
         </div>
-        <div className="text-2xl text-blue-700 ml-40 mt-20 flex">
+        <div className="text-2xl text-blue-700 ml-40 mt-10">
           <FontAwesomeIcon className="h-10 mr-5" icon={faFacebookSquare} />
           <FontAwesomeIcon className="h-10 mr-5" icon={faChevronRight} />
           <Link href="/">facebook</Link>
