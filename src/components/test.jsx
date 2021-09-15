4099ed7b72fa5ec213557bda58a33b7f61b10613
jungle379 <jungle379@gmail.com>
2021-09-15 22:17:50 +0900

    🔥 swrについてコードを書いてみた

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,21 +1,51 @@
-import useSWR from 'swr'
+import useSWR from 'swr';
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("エラーが発生しています");
+  }
+
+  const json = await response.json();
+  return json;
+};
+
+const usePosts = () => {
+  const { data, error } = useSWR("https://jsonplaceholder.typicode.com/postaaa",
+    fetcher);
+  return {
+    data,
+    error,
+    isLoading: !error && !data,
+    isEmpty: data && data.length === 0,
+
+  };
+}
+
 
 const Test = () => {
-  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/posts')
-  console.log({ data, error });
+  const { data, error, isLoading, isEmpty } = usePosts()
 
-  if (!error && !data) {
+  if (isLoading) {
     return <div>ローディング中</div>;
   }
 
   if (error) {
-    return <div>(error.message)</div>;
+    return <div>{error.message}</div>;
   }
 
-  if (data === 0) {
+  if (isEmpty) {
     return <div>データは空です</div>;
   }
 
+  return (
+    <ol>
+      {data.map((post) => {
+        return <li key={post.id}>{post.title}</li>;
+      })}
+    </ol >
+  );
 };
 
 export default Test;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Footer } from '../components/footer'
 import { Main } from "../components/main";
+import Test from '../components/test';
 
 const Home = () => {
   return <>
@@ -10,6 +11,7 @@ const Home = () => {
     <link rel="icon" href="/favicon.ico" />
     <div className="h-auto sm:bg-green-100 bg-green-300">
       <Main />
+      <Test />
     </div>
     <Footer />
   </>
