2584d880d06afb0ac14d2dfdf734b8fd1aa87d4b
jungle379 <jungle379@gmail.com>
2021-09-18 17:03:36 +0900

    ⚡️ ユーザ管理に向けた編集

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,13 +7,13 @@ export function Header() {
         <a className="sm:text-6xl sm:text-red-600 w-2/3">Latest Movie Checker</a>
       </Link>
       <Link href="/header/beginner">
-        <a className="flex-auto text-0.5xl sm:text-1xl ml-10 m-5 w-60 sm:text-pink-300 underline">初めての方へ</a>
+        <a className="flex-auto text-0.5xl sm:text-1xl ml-10 m-5 w-60 hover:text-red-600 underline">初めての方へ</a>
       </Link>
       <Link href="/header/creater">
-        <a className="flex-auto text-0.5xl sm:text-1xl ml-10 m-5 w-60 sm:text-pink-300 underline">作成者について</a>
+        <a className="flex-auto text-0.5xl sm:text-1xl ml-10 m-5 w-60 hover:text-red-600 underline">作成者について</a>
       </Link>
       <Link href="../category">
-        <a className="flex-auto text-0.5xl sm:text-1xl ml-10 m-5 w-80 sm:text-pink-300 underline">どんなジャンルが好き？</a>
+        <a className="flex-auto text-0.5xl sm:text-1xl ml-10 m-5 w-80 hover:text-red-600 underline">マイページへ</a>
       </Link>
     </div>
   );
