cb81e12c9bf30cfe723c7031ec4bc0f46645cd35
jungle379 <jungle379@gmail.com>
2021-09-28 22:14:58 +0900

    ⚡️

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -26,7 +26,7 @@ export function Main() {
             <Link href="/newest/news1">・最新の情報一覧</Link>
           </h2>
           <h3 className="mt-10 sm:ml-14 sm:text-2xl sm:w-100 text-blue-500 underline">
-            <Link href="/newest/co-news">・過去の情報</Link>
+            <Link href="/newest/ex-news">・過去の情報</Link>
           </h3>
         </div>
         <div className="sm:text-2xl w-60 sm:ml-10 sm:mt-20 sm:mb-20 pt-10 sm:w-80 text-blue-500 underline">
