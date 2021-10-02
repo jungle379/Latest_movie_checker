0fc1a7ab5267afe6eb31d324dd48203371b614b9
jungle379 <jungle379@gmail.com>
2021-10-03 07:13:45 +0900

    🐛

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -19,29 +19,31 @@ export function Main() {
           <a className="bg-center w-1/4 text-center bg-gray-200 text-0.5xl sm:text-3xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400 border-2 border-gray-600">シネコン一覧へ</a>
         </Link>
       </div>
-      <div className="sm:flex justify-around mt-20 h-80 sm:border-dashed sm:border-8 sm:border-red-400 mr-40 ml-40">
-        <div>
-          <h1 className="sm:text-4xl mt-10">最新情報</h1>
-          <h2 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
-            <Link href="/newest/news1">・最新の情報一覧</Link>
-          </h2>
-          <h3 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
-            <Link href="/newest/ex-news">・過去の情報</Link>
-          </h3>
+      <div className="flex">
+        <div className="sm:flex justify-around mt-20 h-80 sm:border-dashed sm:border-8 sm:border-red-400 mr-40 ml-40 pl-10 pr-10">
+          <div>
+            <h1 className="sm:text-4xl mt-10">最新情報</h1>
+            <h2 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
+              <Link href="/newest/news1">・最新の情報一覧</Link>
+            </h2>
+            <h3 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
+              <Link href="/newest/ex-news">・過去の情報</Link>
+            </h3>
+          </div>
+          <div className="sm:text-2xl sm:ml-10 sm:mt-20 sm:mb-20 pt-10 text-blue-500 underline">
+            <Link href="/maintainance">・メンテナンス情報</Link>
+          </div>
         </div>
-        <div className="sm:text-2xl sm:ml-10 sm:mt-20 sm:mb-20 pt-10 text-blue-500 underline">
-          <Link href="/maintainance">・メンテナンス情報</Link>
+        <div className="mt-10">
+          <Link href="../category">
+            <h1 className="text-2xl sm:text-1xl mt-10 ml-10 m-5 w-20 underline hover:text-red-500 invisible sm:visible">ジャンル別オススメ映画</h1>
+          </Link>
+          <Link href="/">
+            <h1 className="text-2xl sm:text-1xl mt-10 ml-10 m-5 w-20 underline hover:text-red-500 invisible sm:visible">アカデミー賞候補</h1>
+          </Link>
         </div>
       </div>
-      <div className="text-0.5xl text-yellow-800 float-right mt-0.5 mr-40 sm:visible invisible">updated in 2021/10/01</div>
-      <div className="mt-10 flex">
-        <Link href="../category">
-          <h1 className="text-2xl sm:text-1xl mt-10 ml-40 m-5 w-80 underline hover:text-red-500 invisible sm:visible">ジャンル別オススメ映画</h1>
-        </Link>
-        <Link href="/">
-          <h1 className="text-2xl sm:text-1xl mt-10 ml-40 m-5 w-80 underline hover:text-red-500 invisible sm:visible">アカデミー賞候補</h1>
-        </Link>
-      </div>
+      <div className="text-0.5xl text-yellow-800 mt-0.5 ml-40 sm:visible invisible">updated in 2021/10/01</div>
     </>
   );
 }
