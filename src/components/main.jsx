62b155bab1f5070ba6af1821f4066d93d537947a
jungle379 <jungle379@gmail.com>
2021-10-04 21:04:17 +0900

    🔥 syuusei

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -19,20 +19,18 @@ export function Main() {
           <a className="bg-center w-1/4 text-center bg-gray-200 text-0.5xl sm:text-3xl pt-5 pl-5 pr-5 hover:text-red-500 border-solid sm:border-4 sm:border-gray-400 border-2 border-gray-600">シネコン一覧へ</a>
         </Link>
       </div>
-      <div className="flex">
-        <div className="sm:flex justify-around mt-20 h-80 sm:border-dashed sm:border-8 sm:border-red-400 mr-40 ml-40 pl-10 pr-10">
-          <div>
-            <h1 className="sm:text-4xl mt-10">最新情報</h1>
-            <h2 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
-              <Link href="/newest/news1">・最新の情報一覧</Link>
-            </h2>
-            <h3 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
-              <Link href="/newest/ex-news">・過去の情報</Link>
-            </h3>
-          </div>
-          <div className="sm:text-2xl sm:ml-10 sm:mt-20 sm:mb-20 pt-10 text-blue-500 underline">
+      <div className="sm:flex justify-around mt-20 h-80 sm:border-dashed sm:border-8 sm:border-red-400 mr-40 ml-40 pl-10 pr-10">
+        <div>
+          <h1 className="sm:text-4xl mt-10">最新情報</h1>
+          <h2 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
+            <Link href="/newest/news1">・最新の情報一覧</Link>
+          </h2>
+          <h3 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
+            <Link href="/newest/ex-news">・過去の情報</Link>
+          </h3>
+          <h4 className="mt-10 sm:text-2xl sm:w-100 text-blue-500 underline">
             <Link href="/maintainance">・メンテナンス情報</Link>
-          </div>
+          </h4>
         </div>
         <div className="mt-10">
           <Link href="../category">
