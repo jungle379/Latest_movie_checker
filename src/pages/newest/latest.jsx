d7d83f606bf2cbeac51a8e0090a0c0ad0e5cce44
jungle379 <jungle379@gmail.com>
2021-10-23 20:20:24 +0900

    修正

--- src/pages/newest/latest.jsx
+++ src/pages/newest/latest.jsx
@@ -13,13 +13,13 @@ export default function Latest() {
         <Header />
         <div>
           <h1 className="sm:text-4xl pb-5 text-blue-800 mt-20 ml-10 sm:ml-20">今週公開の映画一覧</h1>
-          <h2 className="sm:text-2xl mt-10 ml-10 sm:ml-40 text-indigo-500 hover:text-red-700">
+          <h2 className="sm:text-2xl mt-10 ml-10 sm:ml-40 text-indigo-500 hover:text-red-500">
             <Link href="https://wildspeed-official.jp">・ワイルドスピード/ジェットブレイク(公開中)</Link>
           </h2>
-          <h3 className="sm:text-2xl mt-10 ml-10 sm:ml-40 text-indigo-500 hover:text-red-700">
+          <h3 className="sm:text-2xl mt-10 ml-10 sm:ml-40 text-indigo-500 hover:text-red-500">
             <Link href="https://anime.fate-go.jp/ep7-tv/">・Fate/GrandOrder(公開中)</Link>
           </h3>
-          <h3 className="sm:text-2xl mt-10 ml-10 sm:ml-40 text-indigo-500 hover:text-red-700">
+          <h3 className="sm:text-2xl mt-10 ml-10 sm:ml-40 text-indigo-500 hover:text-red-500">
             <Link href="https://anime.fate-go.jp/ep7-tv/">・かぐや様は告らせたい(公開中)</Link>
           </h3>
         </div>
