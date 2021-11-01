e53f2e1209cadb0f0c43b4fc6f8440f96cbba377
jungle379 <jungle379@gmail.com>
2021-11-01 22:45:06 +0900

    ⚡️ ページ作成

--- src/pages/corona.jsx
+++ src/pages/corona.jsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { Footer } from "../components/footer";
+import { Header } from "../components/header";
 
 const Corona = () => {
   return (
@@ -6,7 +8,39 @@ const Corona = () => {
       <Head>
         <title>コロナウイルスに対する対策</title>
       </Head>
-      <div>新型コロナウイルスに対する劇場の対策</div>
+      <div className="sm:h-screen sm:bg-green-100 bg-green-300">
+        <Header />
+        <div className="text-4xl text-blue-600 mx-20 my-10">
+          新型コロナウイルスに対する劇場の対策
+        </div>
+        <div className="mx-40 my-10">
+          <li>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, rem?
+            Ab voluptas accusantium distinctio architecto porro quisquam culpa.
+            Iure eaque numquam nostrum saepe quibusdam sed commodi, nisi
+            obcaecati possimus sequi.
+          </li>
+          <li>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            soluta maxime ratione, aspernatur deleniti quasi adipisci assumenda
+            officia reprehenderit sed veniam. Perspiciatis facilis cupiditate
+            quas inventore molestiae, in saepe expedita.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            alias quasi, cumque dolore labore pariatur, beatae quae reiciendis
+            nostrum, temporibus minima. Tenetur alias ratione dolores error a
+            sed ea obcaecati.
+          </li>
+          <li>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Voluptatum, voluptatem. Ipsam maxime minus eius, nobis rerum fugiat
+            dolorem dignissimos atque repellat neque aliquam reiciendis
+            consectetur consequuntur quia eum tempore explicabo.
+          </li>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 };
