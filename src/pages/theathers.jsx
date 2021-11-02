4a0172e9e28f7bf79a782ba8f185fe19d72f6af9
jungle379 <jungle379@gmail.com>
2021-11-02 22:59:53 +0900

    ⚡️

--- src/pages/theathers.jsx
+++ src/pages/theathers.jsx
@@ -6,51 +6,58 @@ import { Footer } from "../components/footer";
 const ITEMS = [
   {
     href: "https://www.tohotheater.jp/index.html",
-    title: "TOHOシネマズ"
-  }, {
+    title: "TOHOシネマズ",
+  },
+  {
     href: "https://109cinemas.net",
-    title: "109シネマズ"
-  }, {
+    title: "109シネマズ",
+  },
+  {
     href: "https://tjoy.jp/shinjuku_wald9",
-    title: "新宿バルト9"
-  }, {
+    title: "新宿バルト9",
+  },
+  {
     href: "https://www.aeoncinema.com",
-    title: "イオンモールシネマ"
-  }, {
+    title: "イオンモールシネマ",
+  },
+  {
     href: "https://www.osakastationcitycinema.com/site/oscc/",
-    title: "大阪ステーションシネマ"
-  }, {
+    title: "大阪ステーションシネマ",
+  },
+  {
     href: "https://ttcg.jp/#/",
-    title: "テアトル系劇場"
-  }, {
+    title: "テアトル系劇場",
+  },
+  {
     href: "https://tjoy.jp/umeda_burg7",
-    title: "梅田ブルク７"
-  }
+    title: "梅田ブルク７",
+  },
 ];
 
 const Others = () => {
-  return <>
-    <Head>
-      <title>主なシネコン一覧</title>
-    </Head>
-    <div className="h-screen sm:bg-green-100 bg-green-300">
-      <Header />
-      <div className="text-4xl text-blue-800 ml-10 mt-20">シネコン一覧</div>
-      <div>
-        {ITEMS.map(item => {
-          return (
-            <div className="sm:text-2xl text-blue-500 hover:text-red-700 pt-10 pl-10">
-              <Link href={item.href}>
-                <li>{item.title}</li>
-              </Link>
-            </div>
-          )
-        })}
+  return (
+    <>
+      <Head>
+        <title>主なシネコン一覧</title>
+      </Head>
+      <div className="h-screen sm:bg-green-100 bg-green-300">
+        <Header />
+        <div className="text-4xl text-blue-800 ml-10 mt-20">シネコン一覧</div>
+        <div>
+          {ITEMS.map((item) => {
+            return (
+              <div className="text-blue-500 hover:text-red-700 mx-20 my-10 w-52">
+                <Link href={item.href}>
+                  <a>{item.title}</a>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  </>
+    </>
+  );
 };
-
 
 export default Others;
