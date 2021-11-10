f6353da92f248bb50d39a5c3e0c419913eaec026
jungle379 <jungle379@gmail.com>
2021-11-10 22:04:13 +0900

    ⚡️ ページ作成

---
+++ src/pages/prise.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Prise = () => {
+  return (
+    <>
+      <Head>
+        <title>アカデミー賞候補の作品一覧</title>
+      </Head>
+      <div>sample</div>
+    </>
+  );
+};
+
+export default Prise;
