1b7310ed6061d9b265aad70a6f6ae9e9acda859a
jungle379 <jungle379@gmail.com>
2021-12-31 17:59:03 +0900

    🔥

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -82,10 +82,10 @@ export function Main() {
         updated in 2022/1/1
       </div>
       {/* valtioを使用した状態管理によるカウントアップ */}
-      <div className="mt-5 mx-10 text-2xl">
+      {/* <div className="mt-5 mx-10 text-2xl">
         {snap.count}
         <button onClick={() => ++state.count}>count up!</button>
-      </div>
+      </div> */}
     </>
   );
 }
