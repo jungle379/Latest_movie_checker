17fe994d2300753cc79426b22d75710de1e01a50
jungle379 <jungle379@gmail.com>
2021-12-31 22:52:58 +0900

    ⚡️ syuusei

--- src/pages/cat/action.jsx
+++ src/pages/cat/action.jsx
@@ -3,21 +3,31 @@ import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
 
 const Action = () => {
-
-  return <>
-    <Head>
-      <title>オススメのアクション映画</title>
-    </Head>
-    <div className="h-screen sm:bg-green-100">
-      <Header />
-      <div className="text-2xl mt-20 ml-10">オススメリスト</div>
-      <h1 className="pt-10 pl-5">規約</h1>
-      <li className="ml-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi velit eius illo reprehenderit animi, at iure tempore pariatur tempora facere deleniti rerum recusandae magnam? Perferendis facilis tenetur distinctio assumenda nemo?</li>
-      <li className="ml-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, delectus cum doloremque aut nobis quia ab est neque harum! Repellat non modi repellendus doloremque sint eligendi facilis cupiditate omnis. Reprehenderit.</li>
-    </div>
-    <Footer />
-
-  </>
+  return (
+    <>
+      <Head>
+        <title>オススメのアクション映画</title>
+      </Head>
+      <div className="h-screen sm:bg-green-100">
+        <Header />
+        <div className="text-2xl mt-20 ml-10">オススメリスト</div>
+        <h1 className="pt-10 pl-5">アクション</h1>
+        <li className="ml-10">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+          velit eius illo reprehenderit animi, at iure tempore pariatur tempora
+          facere deleniti rerum recusandae magnam? Perferendis facilis tenetur
+          distinctio assumenda nemo?
+        </li>
+        <li className="ml-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+          delectus cum doloremque aut nobis quia ab est neque harum! Repellat
+          non modi repellendus doloremque sint eligendi facilis cupiditate
+          omnis. Reprehenderit.
+        </li>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Action;
--- src/pages/cat/musical.jsx
+++ src/pages/cat/musical.jsx
@@ -11,13 +11,12 @@ const Musical = () => {
     <div className="h-screen sm:bg-green-100">
       <Header />
       <div className="text-2xl mt-20 ml-10">オススメリスト</div>
-      <h1 className="pt-10 pl-5">規約</h1>
+      <h1 className="pt-10 pl-5">ミュージカル</h1>
       <li className="ml-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi velit eius illo reprehenderit animi, at iure tempore pariatur tempora facere deleniti rerum recusandae magnam? Perferendis facilis tenetur distinctio assumenda nemo?</li>
       <li className="ml-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eveniet vitae magni nesciunt corrupti, commodi, ea a harum mollitia omnis, incidunt nam architecto repellat maxime recusandae quam voluptatum autem distinctio!</li>
       <li className="ml-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus natus voluptate molestias. Dolorem facere quia praesentium vel laborum deleniti, magni veniam, vero accusamus velit officiis dolore sapiente molestiae ea autem.</li>
     </div>
     <Footer />
-
   </>
 };
 
